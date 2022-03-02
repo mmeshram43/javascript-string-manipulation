1c9224b740809a59fa296293bd57a274e0b3ab2c
Mayur Meshram <mmeshram43@gmail.com>
2022-03-02 12:41:10 +0530

    **

--- src/info.js
+++ src/info.js
@@ -4,7 +4,7 @@ const usage = "Usage: crlf-convert [CR|LF|CRLF]";
 const help = String.raw`${usage}
 
          Converts text between newline (\n), carriage return (\r), and combined
-          "(\r\n) line endings.
+          (\r\n) line endings.
 
           Options:
 
