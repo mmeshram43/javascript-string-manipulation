dbe594859f9950aea02cb60e99ee3c2d2b09e637
Mayur Meshram <mmeshram43@gmail.com>
2022-03-02 12:33:25 +0530

    **

--- src/info.js
+++ src/info.js
@@ -1,6 +1,7 @@
-var usage = "Usage: crlf-convert [CR|LF|CRLF]";
+let usage = "Usage: crlf-convert [CR|LF|CRLF]";
 
-var help = "Usage: crlf-convert [CR|LF|CRLF]\n\n"
+
+let help = "Usage: crlf-convert [CR|LF|CRLF]\n\n"
          + "Converts text between newline (\\n), carriage return (\\r), and combined\n"
          + "(\\r\\n) line endings.\n\n"
          + "Options:\n\n"
